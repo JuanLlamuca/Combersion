1b82dece3a74cafc048c14a3de37c2b462c9dec1
Juan Llamuca <120286570+JuanLlamuca@users.noreply.github.com>
2023-06-01 22:36:52 -0500

    Funcion conversionEuros

--- app/src/main/java/com/example/combersion/Euros.java
+++ app/src/main/java/com/example/combersion/Euros.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Euros extends AppCompatActivity {
 
@@ -16,5 +18,24 @@ public class Euros extends AppCompatActivity {
     //funcion de Finalizar
     public void Finalizar (View view){
         this.finish();
+    }
+
+    //Funcion de Conversion
+    public void ConversionEuros(View view){
+        EditText euros=(EditText) findViewById(R.id.txt_euros);
+        float tipoCambio = 1.08f;
+
+        Float total=Float.parseFloat(euros.getText().toString())*tipoCambio;
+        TextView to=findViewById(R.id.txt_dolares);
+        to.setText(total+"");
+    }
+
+    public void ConversionDolares(View view){
+        EditText dolares=(EditText) findViewById(R.id.txt_dolaresP);
+        float tipoCambio = 0.92f;
+
+        Float total=Float.parseFloat(dolares.getText().toString())*tipoCambio;
+        TextView to=findViewById(R.id.txt_euros2);
+        to.setText(total+"");
     }
 }
