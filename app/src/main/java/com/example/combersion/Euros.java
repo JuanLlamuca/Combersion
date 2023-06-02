d5a8cd45290e959a871d6007ac35b92c2c00e652
Juan Llamuca <120286570+JuanLlamuca@users.noreply.github.com>
2023-06-01 20:49:34 -0500

    Activity Euro

---
+++ app/src/main/java/com/example/combersion/Euros.java
@@ -0,0 +1,14 @@
+package com.example.combersion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Euros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_euros);
+    }
+}
