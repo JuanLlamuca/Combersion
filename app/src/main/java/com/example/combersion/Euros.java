12c1685c49bf37a831eef8724a907febb15c3373
Juan Llamuca <120286570+JuanLlamuca@users.noreply.github.com>
2023-06-01 21:57:19 -0500

    Funcion Finalizar

--- app/src/main/java/com/example/combersion/Euros.java
+++ app/src/main/java/com/example/combersion/Euros.java
@@ -3,6 +3,7 @@ package com.example.combersion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Euros extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class Euros extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_euros);
+    }
+
+    //funcion de Finalizar
+    public void Finalizar (View view){
+        this.finish();
     }
 }
--- app/src/main/java/com/example/combersion/MainActivity.java
+++ app/src/main/java/com/example/combersion/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.combersion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Conversion de Euros a dolares
+    public void irEuro(View view){
+        Intent intent=new Intent(this,Euros.class);
+        view.getContext().startActivity(intent);
     }
 }
